da3db5ac8d80da5078999c29002b5586a24b03d5
Kris Hamoud <khamoud617@gmail.com>
2013-07-24 09:42:50 -0700

    payments

--- app/models/dare.rb
+++ app/models/dare.rb
@@ -3,4 +3,13 @@ class Dare < ActiveRecord::Base
   has_many :backers
   has_many :challenges
   attr_accessible :completed, :current_price, :days_passed, :description, :funding_period, :target_price, :title
+
+  def percent
+  	average = (self.current_price/self.target_price)*100
+  	if average >= 100
+  		100
+  	else
+  		average
+  	end
+  end
 end
