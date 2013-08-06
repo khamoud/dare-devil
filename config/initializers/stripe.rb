fd9b46fb4e6c89c34e66179277b0729ca59cf083
Kris Hamoud <khamoud617@gmail.com>
2013-08-06 13:41:05 -0700

    trying

--- config/initializers/stripe.rb
+++
@@ -1,7 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_live_sGENkZPNEiNKfO64wcut0zJq'],
-  :secret_key      => ENV['sk_live_oiK03QDHKHc1ZbaO09ExY4rW']
-}
-
-Stripe.api_key = ENV['sk_live_oiK03QDHKHc1ZbaO09ExY4rW']
-STRIPE_PUBLIC_KEY = ENV["pk_live_sGENkZPNEiNKfO64wcut0zJq"]
